63a8b0ec36f5d2e5931dcf5a668335ccc3e626eb
Alberto Marchetti <cmaster11@gmail.com>
2019-12-20 11:15:06 +0200

    wip

---
+++ example.py
@@ -0,0 +1,34 @@
+# %%
+
+import numpy as np, pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({'figure.figsize': (9, 7), 'figure.dpi': 120})
+
+# Import data
+df = pd.read_csv('hpa.csv', names=['time', 'value'], header=None)
+
+minTime = min(df.time)
+maxTime = max(df.time)
+
+# Original Series
+fig, axes = plt.subplots(3, 2, sharex=True)
+axes[0, 0].set_xlim(left=minTime, right=maxTime)
+axes[0, 0].plot(df.time, df.value)
+axes[0, 0].set_title('Original Series')
+plot_acf(df.value, ax=axes[0, 1])
+
+# 1st Differencing
+axes[1, 0].set_xlim(left=minTime, right=maxTime)
+axes[1, 0].plot(df.time, df.value.diff())
+axes[1, 0].set_title('1st Order Differencing')
+plot_acf(df.value.diff().dropna(), ax=axes[1, 1])
+
+# 2nd Differencing
+axes[2, 0].set_xlim(left=minTime, right=maxTime)
+axes[2, 0].plot(df.time, df.value.diff().diff())
+axes[2, 0].set_title('2nd Order Differencing')
+plot_acf(df.value.diff().diff().dropna(), ax=axes[2, 1])
+
+plt.show()
